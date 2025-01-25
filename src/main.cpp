1bd72e6926282ed8d52e8984337afddc1b99ab3a
ArtemiiAkhunov <artemii13052003@gmail.com>
2025-01-24 22:51:30 -0500

    Figured animation

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,73 @@
 #include "bn_core.h"
+#include "bn_keypad.h"
+#include "bn_sprite_items_astronaut.h"
+#include "bn_sprite_items_ninja.h"
+#include "bn_sprite_actions.h"
+#include "bn_bg_palettes.h"
+#include "bn_sprite_ptr.h"
+#include "bn_sprite_animate_actions.h"
 
-int main()
-{
-    bn::core::init();
+#include "bn_camera_ptr.h"
 
-    while(true)
+void sprites_visibility_scene()
+  {
+
+      bn::sprite_ptr red_sprite = bn::sprite_items::astronaut.create_sprite(0, 0);
+
+      while(! bn::keypad::start_pressed())
+      {
+          if(bn::keypad::a_pressed())
+          {
+              red_sprite.set_visible(! red_sprite.visible());
+          }
+
+          bn::core::update();
+      }
+  }
+
+void animation() {
+  bn::sprite_ptr ninja_sprite = bn::sprite_items::astronaut.create_sprite(0, 0);
+
+  bn::sprite_animate_action<2> action = bn::create_sprite_animate_action_forever(
+    ninja_sprite, 2, bn::sprite_items::astronaut.tiles_item(), 1, 0);
+
+  while(! bn::keypad::start_pressed()) {
+    if(bn::keypad::left_pressed())
     {
-        bn::core::update();
-    }
+                action = bn::create_sprite_animate_action_forever(
+                            ninja_sprite, 16, bn::sprite_items::astronaut.tiles_item(), 1, 0);
+            }
+            else if(bn::keypad::right_pressed())
+            {
+                action = bn::create_sprite_animate_action_forever(
+                            ninja_sprite, 16, bn::sprite_items::astronaut.tiles_item(), 1, 0);
+            }
+
+            if(bn::keypad::up_pressed())
+            {
+                action = bn::create_sprite_animate_action_forever(
+                            ninja_sprite, 16, bn::sprite_items::astronaut.tiles_item(), 0, 1);
+            }
+            else if(bn::keypad::down_pressed())
+            {
+                action = bn::create_sprite_animate_action_forever(
+                            ninja_sprite, 16, bn::sprite_items::astronaut.tiles_item(), 0, 1);
+            }
+
+            action.update();
+            bn::core::update();
+  }
+
+}
+
+int main() {
+
+  bn::core::init();
+  bn::bg_palettes::set_transparent_color(bn::color(16, 16, 16));
+
+  while(true)
+  {
+    animation();
+    bn::core::update();
+  }
 }
