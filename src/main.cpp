7e2ce7a90fddd38dc92e1c41aa73c1e3ec5dee92
jcrm1 <52137472+jcrm1@users.noreply.github.com>
2025-01-25 13:59:13 -0500

    sky regular_bg

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include "bn_sprite_items_astronaut.h"
 #include "bn_sprite_items_ninja.h"
 #include "bn_sprite_ptr.h"
-#include "bn_regular_bg_items_blue_bg.h"
+#include "bn_regular_bg_items_sky.h"
 
 #include "bn_camera_ptr.h"
 #include "bn_music_items.h"
@@ -16,19 +16,19 @@
 #include "bn_timers.h"
 
 void sprites_bg_move_scene() {
-  bn::regular_bg_ptr blue_bg = bn::regular_bg_items::blue_bg.create_bg(-56, -56);
-  blue_bg.set_priority(0);
+  bn::regular_bg_ptr sky = bn::regular_bg_items::sky.create_bg(-56, -56);
+  sky.set_priority(0);
   int speed = 1;
 
   while(! bn::keypad::start_pressed()) {
     if (bn::keypad::left_held()) {
-      blue_bg.set_x(blue_bg.x() - speed);
+      sky.set_x(sky.x() - speed);
     } else if (bn::keypad::right_held()) {
-      blue_bg.set_x(blue_bg.x() + speed);
+      sky.set_x(sky.x() + speed);
     } else if (bn::keypad::up_held()) {
-      blue_bg.set_y(blue_bg.y() + speed);
+      sky.set_y(sky.y() + speed);
     } else if (bn::keypad::down_held()) {
-      blue_bg.set_y(blue_bg.y() - speed);
+      sky.set_y(sky.y() - speed);
     }
     bn::core::update();
   }
